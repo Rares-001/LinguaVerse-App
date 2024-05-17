3ea33e0eca1727194018d93dc8d3f7d1ff1be661
lele.husarescu1@student.nhlstenden.com <lele.husarescu1@student.nhlstenden.com>
2024-05-17 12:26:16 +0200

    Aggiungere i file di progetto.

---
+++ LinguaVerse-App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LinguaVerse_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}

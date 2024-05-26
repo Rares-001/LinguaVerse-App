cf8b7a9dcc1f68c70302539c027a56c30351f083
Polina <91316935+Polinalavender@users.noreply.github.com>
2024-05-26 06:27:11 -0700

    Add registration model view

---
+++ LinguaVerse-App/ViewModels/Registration.cs
@@ -0,0 +1,15 @@
+namespace LinguaVerse_App.ViewModels;
+
+public class Registration : ContentView
+{
+	public Registration()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}

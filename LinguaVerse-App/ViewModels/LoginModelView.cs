1503ce55faba48d11216b02103da6a158013a437
Polina <91316935+Polinalavender@users.noreply.github.com>
2024-05-26 05:41:42 -0700

    add login model view

--- LinguaVerse-App/ViewModels/LoginModelView.cs
+++ LinguaVerse-App/ViewModels/LoginModelView.cs
@@ -60,4 +60,19 @@ public partial class LoginModelView : ContentView
     }
 }
        
-     
+
+namespace LinguaVerse_App.ViewModels;
+
+public class LoginModelView : ContentView
+{
+	public LoginModelView()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}

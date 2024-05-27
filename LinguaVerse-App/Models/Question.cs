d94024c62d08ee51bc6ae4466757fc892d80666c
lele.husarescu1@student.nhlstenden.com <lele.husarescu1@student.nhlstenden.com>
2024-05-27 09:33:43 +0200

    Commit for backup purposes, transitioning from Parallels to VMWare Fusion for WindowsCommit for backup purposes, transitioning from Parallels to VMWare Fusion for Windows

---
+++ LinguaVerse-App/Models/Question.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinguaVerse_App.Models
+{
+    public class Question
+    {
+        public string QuestionText { get; set; }
+        public string Answer { get; set; }
+        public ObservableCollection<string> Choices { get; set; }
+    }
+}
+
---
+++ LinguaVerse-App/ViewModel/QuizViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using LinguaVerse_App.Models;
+using System.Linq;
+
+namespace LinguaVerse_App.ViewModels
+{
+    public class QuizViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Question> Questions { get; set; }
+        private string _result;
+
+        public string Result
+        {
+            get => _result;
+            set
+            {
+                _result = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Command CheckAnswersCommand { get; }
+
+        public QuizViewModel()
+        {
+            Questions = new ObservableCollection<Question>
+            {
+                new Question
+                {
+                    QuestionText = "Are you a teacher?",
+                    Answer = "Yes",
+                    Choices = new ObservableCollection<string> { "Yes", "No" }
+                },
+                new Question
+                {
+                    QuestionText = "Is your name Marcus?",
+                    Answer = "Yes",
+                    Choices = new ObservableCollection<string> { "Yes", "No" }
+                },
+                new Question
+                {
+                    QuestionText = "Are your children here?",
+                    Answer = "No, they aren't",
+                    Choices = new ObservableCollection<string> { "Yes, they are", "No, they aren't" }
+                },
+                // Add more questions as needed
+            };
+
+            CheckAnswersCommand = new Command(CheckAnswers);
+        }
+
+        private void CheckAnswers()
+        {
+            bool allCorrect = Questions.All(q => q.Answer == q.Choices[0]); 
+            Result = allCorrect ? "Correct" : "Wrong";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
